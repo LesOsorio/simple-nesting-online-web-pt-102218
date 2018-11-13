7bcbcb1feac5bf802ac035910716f1085d339f56
Leslie Osorio <lesosorio17@gmail.com>
2018-11-13 20:38:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      [:alan_kay][:known_for] << "GUI"
+	 end 
 		 programmer_hash
-
 end
 
 def adding_to_dennis
